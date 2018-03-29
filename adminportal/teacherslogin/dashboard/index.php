b249b30fb936ca29aefb4a18da8cf9c34c9f9e3c
liyasthomas <liyascthomas@gmail.com>
2018-03-29 08:38:30 +0530

    UI optimizations

--- adminportal/teacherslogin/dashboard/index.php
+++ adminportal/teacherslogin/dashboard/index.php
@@ -35,7 +35,7 @@
 				while($row = $result->fetch_assoc()){
 	?>
 				 <div class="flex">
-				 <div class="subheading flex"><?php echo $row["sub_name"]; ?> (<?php echo $row["class"]; ?>)</div>
+				 <div class="subheading" style="width:100%"><?php echo $row["sub_name"]; ?> (<?php echo $row["class"]; ?>)</div>
 					 <div>
 					 <a href="feedshow/?class=<?php echo $row["class"]; ?>">
 					 <div class="button">Open</div>
