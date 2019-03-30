504983d83fcb7a12f88ba80680223361112b8e0a
liyasthomas <liyascthomas@gmail.com>
2019-03-30 12:53:42 +0530

    Cloned hods -> principal in favor of future developments (beta)

---
+++ adminportal/principallogin/dashboard/index.php
@@ -0,0 +1,264 @@
+<html>
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+	<script src="../../../scripts/material.min.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet'>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="../../../styles/material.min.css">
+	<link rel="stylesheet" href="../../../styles/main.css">
+	<link rel="icon" type="image/png" href="../../../images/logo.png">
+	<title>Dashboard | Feedie</title>
+</head>
+
+<body class="mdl-demo mdl-color-text--grey-900 mdl-base">
+	<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+		<header class="mdl-layout__header mdl-color--white mdl-color-text--grey-600">
+			<div class="mdl-layout__header-row">
+				<a href="../">
+					<button class="mdl-button mdl-js-button mdl-button--icon">
+						<i class="material-icons">home</i>
+					</button>
+				</a>
+				<span class="mdl-layout-title">Feedie</span>
+				<div class="mdl-layout-spacer"></div>
+				<span class="mdl-layout-title">
+					<?php
+						include('../../../db_config.php');
+						session_start();
+						if (!isset($_SESSION["hod_username"])){
+							header('Location: ../../');
+						}
+						echo $_SESSION["hod_username"];
+						echo " &middot; ";
+						echo $_SESSION["dept"];
+					?>
+				</span>
+				<button id="more" class="mdl-button mdl-js-button mdl-button--icon">
+					<i class="material-icons">account_circle</i>
+				</button>
+				<ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="more">
+					<a href="../../?logout=1">
+						<li class="mdl-menu__item">
+							<i class="material-icons" role="presentation">exit_to_app</i>
+							Logout
+						</li>
+					</a>
+				</ul>
+			</div>
+			<div class="mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--white">
+				<a href="#overview" class="mdl-layout__tab is-active">Feedback</a>
+				<a href="#account" class="mdl-layout__tab">Profile</a>
+			</div>
+		</header>
+		<main class="mdl-layout__content">
+			<div class="mdl-layout__tab-panel is-active" id="overview">
+				<section class="section--center mdl-card mdl-grid mdl-grid--no-spacing">
+					<h4 class="mdl-cell mdl-cell--12-col">Department faculties</h4>
+					<?php
+						include('../../../db_config.php');
+						$sql = "SELECT te_username FROM teachers WHERE dept = '".$_SESSION["dept"]."'";
+						$result = $conn->query($sql);
+						if($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()){
+					?>
+					<div onclick="location.href='teachersmenu/?te_username=<?php echo $row["te_username"]; ?>'" class="mdl-cell mdl-cell--12-col mdl-grid subjects">
+						<div class="flex-center mdl-cell mdl-cell--1-col">
+							<span class="mdl-color-text--green-a400">
+								<i class="material-icons">done</i>
+							</span>
+						</div>
+						<div class="section__text mdl-cell mdl-cell--11-col-desktop mdl-cell--7-col-tablet mdl-cell--3-col-phone">
+							<h5>
+								<?php echo $row["te_username"]; ?>
+							</h5>
+						</div>
+					</div>
+					<?php
+							}
+						}
+					?>
+					<h4 class="mdl-cell mdl-cell--12-col">Other Department's Collabrating Teachers</h4>
+					<?php
+						include('../../../db_config.php');
+						$sql = "SELECT te_username FROM teachers WHERE dept NOT LIKE '".$_SESSION["dept"]."' AND te_username = ANY (SELECT te_username FROM teachersinfo WHERE class_dept LIKE '".$_SESSION["dept"]."')";
+						$result = $conn->query($sql);
+						if($result->num_rows > 0) {
+							while($row = $result->fetch_assoc()){
+					?>
+					<div onclick="location.href='teachersmenu/?te_username=<?php echo $row["te_username"]; ?>'" class="mdl-cell mdl-cell--12-col mdl-grid subjects">
+						<div class="flex-center mdl-cell mdl-cell--1-col">
+							<span class="mdl-color-text--green-a400">
+								<i class="material-icons">done</i>
+							</span>
+						</div>
+						<div class="section__text mdl-cell mdl-cell--11-col-desktop mdl-cell--7-col-tablet mdl-cell--3-col-phone">
+							<h5>
+								<?php echo $row["te_username"]; ?>
+							</h5>
+						</div>
+					</div>
+					<?php
+							}
+						}
+					?>
+				</section>
+			</div>
+			<div class="mdl-layout__tab-panel" id="account">
+				<section class="section--center mdl-card mdl-grid mdl-grid--no-spacing">
+					<h4 class="mdl-cell mdl-cell--12-col">Account</h4>
+					<div class="mdl-cell mdl-cell--12-col mdl-grid">
+						<div class="flex-center mdl-cell mdl-cell--1-col">
+							<span class="mdl-color-text--red-a400">
+								<i class="material-icons">close</i>
+							</span>
+						</div>
+						<div class="section__text mdl-cell mdl-cell--11-col-desktop mdl-cell--7-col-tablet mdl-cell--3-col-phone">
+							<h5>
+								Name: <span style="text-transform: capitalize;"><?php echo $_SESSION["hod_username"]; ?></span>
+							</h5>
+						</div>
+					</div>
+					<div class="mdl-cell mdl-cell--12-col mdl-grid">
+						<div class="flex-center mdl-cell mdl-cell--1-col">
+							<span class="mdl-color-text--red-a400">
+								<i class="material-icons">close</i>
+							</span>
+						</div>
+						<div class="section__text mdl-cell mdl-cell--11-col-desktop mdl-cell--7-col-tablet mdl-cell--3-col-phone">
+							<h5>
+								Department: <?php echo $_SESSION["dept"]; ?>
+							</h5>
+						</div>
+					</div>
+					<h4 class="mdl-cell mdl-cell--12-col">Change password</h4>
+					<form action="" method="post">
+						<div class="mdl-grid mdl-grid--no-spacing">
+							<div class="flex-center mdl-cell mdl-cell--1-col">
+								<div class="section__circle-container__circle mdl-color-text--amber-300">
+									<i class="material-icons">vpn_key</i>
+								</div>
+							</div>
+							<div class="section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--2-col-phone">
+								<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+									<input class="mdl-textfield__input" type="password" name="currentpassword" id="currentpassword">
+									<label class="mdl-textfield__label">Current password</label>
+								</div>
+							</div>
+							<div class="flex-center mdl-cell mdl-cell--1-col">
+								<span onclick="cptoggler()" class="mdl-button mdl-js-button mdl-button--icon mdl-button--accent">
+									<i id="cp" class="material-icons">visibility_off</i>
+								</span>
+							</div>
+							<div class="flex-center mdl-cell mdl-cell--1-col">
+								<div class="section__circle-container__circle mdl-color-text--green-a400">
+									<i class="material-icons">vpn_key</i>
+								</div>
+							</div>
+							<div class="section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--2-col-phone">
+								<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+									<input class="mdl-textfield__input" type="password" name="newpassword" id="newpassword">
+									<label class="mdl-textfield__label">New password</label>
+								</div>
+							</div>
+							<div class="flex-center mdl-cell mdl-cell--1-col">
+								<span onclick="nptoggler()" class="mdl-button mdl-js-button mdl-button--icon mdl-button--accent">
+									<i id="np" class="material-icons">visibility_off</i>
+								</span>
+							</div>
+							<div class="flex-center mdl-cell mdl-cell--1-col">
+								<div class="section__circle-container__circle mdl-color-text--green-a400">
+									<i class="material-icons">vpn_key</i>
+								</div>
+							</div>
+							<div class="section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--2-col-phone">
+								<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+									<input class="mdl-textfield__input" type="password" name="renewpassword" id="renewpassword">
+									<label class="mdl-textfield__label">Retype new password</label>
+								</div>
+							</div>
+							<div class="flex-center mdl-cell mdl-cell--1-col">
+								<span onclick="rptoggler()" class="mdl-button mdl-js-button mdl-button--icon mdl-button--accent">
+									<i id="rp" class="material-icons">visibility_off</i>
+								</span>
+							</div>
+							<div class="mdl-cell mdl-cell--12-col questions">
+								<button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent">
+									Change password
+								</button>
+							</div>
+							<script>
+								function cptoggler() {
+									if (document.getElementById("cp").innerHTML == 'visibility_off') {
+										document.getElementById("cp").innerHTML = 'visibility';
+										document.getElementById("currentpassword").type = "text";
+									} else {
+										document.getElementById("cp").innerHTML = 'visibility_off';
+										document.getElementById("currentpassword").type = "password";
+									}
+								}
+
+								function nptoggler() {
+									if (document.getElementById("np").innerHTML == 'visibility_off') {
+										document.getElementById("np").innerHTML = 'visibility';
+										document.getElementById("newpassword").type = "text";
+									} else {
+										document.getElementById("np").innerHTML = 'visibility_off';
+										document.getElementById("newpassword").type = "password";
+									}
+								}
+
+								function rptoggler() {
+									if (document.getElementById("rp").innerHTML == 'visibility_off') {
+										document.getElementById("rp").innerHTML = 'visibility';
+										document.getElementById("renewpassword").type = "text";
+									} else {
+										document.getElementById("rp").innerHTML = 'visibility_off';
+										document.getElementById("renewpassword").type = "password";
+									}
+								}
+
+							</script>
+							<div class="mdl-cell mdl-cell--12-col mdl-color-text--red-a400 questions">
+							</div>
+						</div>
+					</form>
+				</section>
+			</div>
+		</main>
+	</div>
+	<div id="snackbar">Some text some message..</div>
+	<?php
+		//Toast setting-up
+		if (isset($_SESSION["toast_type"])) {
+			if($_SESSION["toast_type"] == "update") {
+				echo 
+					'<script type="text/javascript">
+					function showsnackbar() {
+					var x = document.getElementById("snackbar");
+					x.innerHTML = "Response updated";
+					x.className = "show";
+					setTimeout(function(){ x.className = x.className.replace("show", ""); }, 4000);
+					}
+					showsnackbar();
+					</script>';
+					unset($_SESSION["toast_type"]);
+			}
+			elseif ($_SESSION["toast_type"] == "first-time") {
+				echo 
+					'<script type="text/javascript">
+					function showsnackbar() {
+					var x = document.getElementById("snackbar");
+					x.innerHTML = "Response added";
+					x.className = "show";
+					setTimeout(function(){ x.className = x.className.replace("show", ""); }, 4000);
+					}
+					showsnackbar();
+					</script>';
+					unset($_SESSION["toast_type"]);
+			}
+		}
+	?>
+</body>
+
+</html>
---
+++ adminportal/principallogin/dashboard/teachersmenu/scorecard/teachersview/index.php
@@ -0,0 +1,320 @@
+<html>
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" type="text/css" href="../../../../../../css.css">
+	<link href='https://fonts.googleapis.com/css?family=Product+Sans' rel='stylesheet'>
+	<link rel="icon" type="image/png" href="../../../../../../images/logo.png">
+	<title>Feedie | View Feedback</title>
+</head>
+
+<body>
+	<div class="header">
+		<div><a href="../../../"><img src="../../../../../../images/back.svg" class="home"></a></div>
+		<img src="../../../../../../images/logo.svg" class="logo" />
+		<div class="title">Feedie</div>
+		<a href="../../../../../?logout=1">
+			<div class="logout">Logout</div>
+		</a>
+	</div>
+	<div class="details">Subject name:
+		<?php
+    session_start();
+
+    if (!isset($_SESSION["hod_username"])){
+        sleep(1);
+        header('Location: ../../../../../');
+    }
+
+    echo $_SESSION["sub_name"];
+    include('../../../../../../db_config.php');
+    $sql1 = "SELECT COUNT(*) FROM feeds WHERE te_username='".$_SESSION["te_username"]."' AND class='".$_SESSION["class"]."'";
+    $result1 = $conn->query($sql1);
+
+    if($result1->num_rows > 0) {
+      $row1 = $result1->fetch_assoc();
+      echo "<br>No. of feedbacks applied : ".$row1["COUNT(*)"];
+      $feed_no = $row1["COUNT(*)"]; 
+    }
+
+    $sql1 = "SELECT class_strength FROM teachersinfo WHERE te_username='".$_SESSION["te_username"]."' AND class='".$_SESSION["class"]."'";
+    $result1 = $conn->query($sql1);
+    if($result1->num_rows > 0) {
+      $row1 = $result1->fetch_assoc();
+      echo "<br>No. of total students : ".$row1["class_strength"];
+      $tot_no = $row1["class_strength"]; 
+    }
+    
+    echo "<br>No. remaining feedbacks : ".($tot_no - $feed_no);
+
+    $max = 0;
+    $i = 1;
+    $sql1 = "SELECT r_value FROM ratings ORDER BY r_no";
+    $result1 = $conn->query($sql1);
+    if($result1->num_rows > 0) {
+      while($row1 = $result1->fetch_assoc()){
+        $mark[$i] = $row1["r_value"];
+        if( $max < $row1["r_value"] ){
+          $max = $row1["r_value"];
+        } 
+        ++$i;
+      }
+    }
+    $max = $max*$feed_no;
+
+  ?>
+	</div>
+	<div class="page">
+		<div class="container">
+			<div class="phpr">
+				<?php
+        echo "Guidlines:<br>
+	      &#9733 = Poor<br>
+	      &#9733 &#9733 = Need improvements<br>
+	      &#9733 &#9733 &#9733 = Average<br>
+	      &#9733 &#9733 &#9733 &#9733 = Good<br>
+	      &#9733 &#9733 &#9733 &#9733 &#9733 = Excellent";
+		?>
+			</div>
+
+			<?php
+
+  include('../../../../../../db_config.php');
+  $sql_q = "SELECT quest_id, quest_content FROM questions ORDER BY quest_id ASC";
+  $result_q = $conn->query($sql_q);
+  if ( $result_q->num_rows > 0 ){
+    while($row_q = $result_q->fetch_assoc()){
+
+
+  ?>
+
+				<div class="row">
+					<?php  
+        echo $row_q["quest_id"].". ".$row_q["quest_content"]; 
+        $q_no = "q".$row_q["quest_id"];
+    ?>
+				</div>
+				<div class="row">
+					<div class="side">
+						<div>5 stars</div>
+					</div>
+					<div class="middle">
+						<div class="bar-container">
+							<div class="bar-5" style="width:
+      <?php
+        $sql = " SELECT ".$q_no." FROM feeds WHERE te_username='".$_SESSION["te_username"]."' AND class='".$_SESSION["class"]."' AND sub_code='".$_SESSION["sub_code"]."' ";
+        $result = $conn->query($sql);
+        $count = 0;
+        $final = 0;
+        if($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()){
+            if($row[$q_no] == 5){
+              $count = $count+1;
+            } 
+          }
+          $final = ($count/$feed_no)*100;
+          echo $final."% ";
+        }
+      ?>
+      ">
+							</div>
+						</div>
+					</div>
+					<div class="side right">
+						<?php echo $count; ?>
+					</div>
+
+					<div class="side">
+						<div>4 stars</div>
+					</div>
+					<div class="middle">
+						<div class="bar-container">
+							<div class="bar-4" style="width:
+      <?php
+        $sql = " SELECT ".$q_no." FROM feeds WHERE te_username='".$_SESSION["te_username"]."' AND class='".$_SESSION["class"]."' AND sub_code='".$_SESSION["sub_code"]."' ";
+        $result = $conn->query($sql);
+        $count = 0;
+        $final = 0;
+        if($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()){
+            if($row[$q_no] == 4){
+              $count = $count+1;
+            } 
+          }
+          $final = ($count/$feed_no)*100;
+          echo $final."% ";
+        }
+      ?>
+      ">
+							</div>
+						</div>
+					</div>
+					<div class="side right">
+						<?php echo $count; ?>
+					</div>
+
+					<div class="side">
+						<div>3 stars</div>
+					</div>
+					<div class="middle">
+						<div class="bar-container">
+							<div class="bar-3" style="width:
+      <?php
+        $sql = " SELECT ".$q_no." FROM feeds WHERE te_username='".$_SESSION["te_username"]."' AND class='".$_SESSION["class"]."' AND sub_code='".$_SESSION["sub_code"]."' ";
+        $result = $conn->query($sql);
+        $count = 0;
+        $final = 0;
+        if($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()){
+            if($row[$q_no] == 3){
+              $count = $count+1;
+            } 
+          }
+          $final = ($count/$feed_no)*100;
+          echo $final."% ";
+        }
+      ?>
+      ">
+							</div>
+						</div>
+					</div>
+					<div class="side right">
+						<?php echo $count; ?>
+					</div>
+
+					<div class="side">
+						<div>2 stars</div>
+					</div>
+					<div class="middle">
+						<div class="bar-container">
+							<div class="bar-2" style="width:
+      <?php
+        $sql = " SELECT ".$q_no." FROM feeds WHERE te_username='".$_SESSION["te_username"]."' AND class='".$_SESSION["class"]."' AND sub_code='".$_SESSION["sub_code"]."' ";
+        $result = $conn->query($sql);
+        $count = 0;
+        $final = 0;
+        if($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()){
+            if($row[$q_no] == 2){
+              $count = $count+1;
+            } 
+          }
+          $final = ($count/$feed_no)*100;
+          echo $final."% ";
+        }
+      ?>
+      ">
+							</div>
+						</div>
+					</div>
+					<div class="side right">
+						<?php echo $count; ?>
+					</div>
+
+					<div class="side">
+						<div>1 star</div>
+					</div>
+					<div class="middle">
+						<div class="bar-container">
+							<div class="bar-1" style="width: 
+      <?php
+        $sql = " SELECT ".$q_no." FROM feeds WHERE te_username='".$_SESSION["te_username"]."' AND class='".$_SESSION["class"]."' AND sub_code='".$_SESSION["sub_code"]."' ";
+        $result = $conn->query($sql);
+        $count = 0;
+        $final = 0;
+        if($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()){
+            if($row[$q_no] == 1){
+              $count = $count+1;
+            } 
+          }
+          $final = ($count/$feed_no)*100;
+          echo $final."% ";
+        }
+      ?>
+      ">
+							</div>
+						</div>
+					</div>
+					<div class="side right">
+						<?php echo $count; ?>
+					</div>
+
+					<!--Total estimation-->
+					<div class="side">
+						<div>Total</div>
+					</div>
+					<div class="middle">
+						<div class="bar-container">
+							<div class="bar-5" style="width:
+      <?php
+        $sql = " SELECT ".$q_no." FROM feeds WHERE te_username='".$_SESSION["te_username"]."' AND class='".$_SESSION["class"]."' AND sub_code='".$_SESSION["sub_code"]."' ";
+        $result = $conn->query($sql);
+        $count = 0;
+        $final = 0;
+        if($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()){
+            if($row[$q_no] == 5){
+              $count = $count+$mark[5];
+            } 
+            if($row[$q_no] == 4){
+              $count = $count+$mark[4];
+            } 
+            if($row[$q_no] == 3){
+              $count = $count+$mark[3];
+            } 
+            if($row[$q_no] == 2){
+              $count = $count+$mark[2];
+            } 
+            if($row[$q_no] == 1){
+              $count = $count+$mark[1];
+            } 
+          }
+          $final = ($count/$max)*100;
+          $pt[$row_q["quest_id "]] = $final;
+          echo $final."% ";
+        }
+      ?>
+      ">
+							</div>
+						</div>
+					</div>
+					<div class="side right">
+						<?php echo $final; ?>%
+					</div>
+				</div>
+
+				<?php
+    } //while ends
+  }   //if ends
+
+  ?>
+
+					<div class="row">
+						<div class="side">
+							<div>Grand Total</div>
+						</div>
+						<div class="middle">
+							<div class="bar-container">
+								<div class="bar-1" style="width: 
+      <?php
+        $count = 0;
+        $final = 0;
+        $count = $pt[1] + $pt[2] + $pt[3] + $pt[4] + $pt[5] + $pt[6] + $pt[7] + $pt[8] + $pt[9] + $pt[10]; 
+          $final = ($count/1000)*100;
+          echo $final." % ";
+      ?>
+      ">
+								</div>
+							</div>
+						</div>
+						<div class="side right">
+							<?php echo round($final); ?>%
+						</div>
+					</div>
+		</div>
+	</div>
+	<footer><a href="https://fuseorg.github.io/Feedie" class="link" target="_blank">Fuse Org</a></footer>
+</body>
+
+</html>
